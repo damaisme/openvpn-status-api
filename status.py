aa6c64039394f7203f7b79799a45a7c45913f35c
radendi <damasukmath@gmail.com>
2023-09-22 21:11:55 +0700

    fix and add status.py

---
+++ status.py
@@ -0,0 +1,17 @@
+import pprint
+from openvpn_status_parser import OpenVPNStatusParser
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/api/openvpn/client-list', methods=['GET'])
+def get_openvpn_client_list():
+
+    parser = OpenVPNStatusParser("/var/log/openvpn/openvpn-status.log")
+    parsed_data = parser.connected_clients
+    return jsonify(parsed_data)
+    #pprint.pprint(parser.routing_table)
+    #pprint.pprint(parser.details)
+
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0", port="65080")
