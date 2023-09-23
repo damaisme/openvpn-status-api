de921832bb949d60b573728174b12b26954a4019
Damasukma Trihanandi <73756341+radendi@users.noreply.github.com>
2023-09-23 18:31:52 +0700

    Update status.py

--- status.py
+++ status.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/api/openvpn/client-list', methods=['GET'])
 def get_openvpn_client_list():
 
-    parser = OpenVPNStatusParser("/var/log/openvpn/openvpn-status.log")
+    parser = OpenVPNStatusParser("/var/run/openvpn-server/status-server.log")
     parsed_data = parser.connected_clients
     return jsonify(parsed_data)
     #pprint.pprint(parser.routing_table)
